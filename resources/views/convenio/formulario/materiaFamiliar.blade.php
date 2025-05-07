cdacef4c38c85a8613872f89a22aba4b6d23eebb
Emanuel Rios Roque <emanuel.roque.web@gmail.com>
2025-05-07 08:42:26 -0600

    Correccion de texto

--- resources/views/convenio/formulario/materiaFamiliar.blade.php
+++ resources/views/convenio/formulario/materiaFamiliar.blade.php
@@ -47,7 +47,9 @@
     <div class="space-y-1">
         <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200">
             Edad 
+            @if($prefix === 'solicitante')
                 <flux:badge color="emerald" size="sm" class="ml-2">Obligatorio</flux:badge>
+            @endif
         </label>
         <flux:input
             wire:model="edad_solicitante"
