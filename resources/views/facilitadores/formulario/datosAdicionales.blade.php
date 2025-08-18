da6dd3b14866636647cbd526f2bc4e0eedb62c84
Emanuel Rios Roque <emanuel.roque.web@gmail.com>
2025-08-18 08:28:44 -0600

    ADD: Se agregan * para aquellos datos que son obligatorios en el formulario para crear un nuevo facilitador

--- resources/views/facilitadores/formulario/datosAdicionales.blade.php
+++ resources/views/facilitadores/formulario/datosAdicionales.blade.php
@@ -1,16 +1,16 @@
 <!-- Sección 1: Datos generales del facilitador -->
 <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2" x-data="">
-    <flux:input wire:model="duracion_encargo" :label="__('Duración del encargo')" type="text" required
+    <flux:input wire:model="duracion_encargo" :label="__('Duración del encargo *')" type="text" required
         placeholder="Duración del encargo" />
     
-    <flux:input wire:model="area_adscrito" :label="__('Área de adscripción territorial')" type="number" required
+    <flux:input wire:model="area_adscrito" :label="__('Área de adscripción territorial *')" type="number" required
         placeholder="Área de adscripción territorial" />
 </div>
 
-
-<div class="space-y-1">
+<!-- Sección 1.1: Numero renovacion -->
+<div class="space-y-1 mt-2">
     <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200">
-        Número de renovaciones de certificación
+        Número de renovaciones de certificación *
     </label>
 
     <div class="flex gap-2">
@@ -48,10 +48,6 @@
     @endif
 </div>
 
-
-
-
-
 <!-- Sección 2: Autoridades y documentos -->
 <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4" x-data="{
         autorizacion: @entangle('autorizacion').live,
@@ -63,7 +59,7 @@
 
     <div>
         <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1">
-            Nombre del Poder Judicial que otorgó la certificación o renovación
+            Nombre del Poder Judicial que otorgó la certificación o renovación *
         </label>
         <flux:select wire:model="autoridad_certificacion" placeholder="Elige una institución">
             <flux:select.option>PJCDMX</flux:select.option>
@@ -73,14 +69,24 @@
 
     <div class=" space-y-1">
         <div x-show="autoridad_certificacion == 'otro'" x-cloak class="mt-4">
-            <flux:input wire:model="especificacion_autoridad" :label="__('Especificar')" type="text" required
-                placeholder="Especificar" />
+            <flux:input 
+                wire:model="especificacion_autoridad" 
+                :label="__('Especificar *')" 
+                type="text" 
+                placeholder="Especificar" 
+            />
         </div>
-        <flux:input wire:model="clave_autoridad" :label="__('Clave de la Entidad Federativa que otorgó la Certificación o renovación ')" type="text" maxlength="4" required placeholder="Clave" />
+        <flux:input 
+            wire:model="clave_autoridad" 
+            :label="__('Clave de la Entidad Federativa que otorgó la Certificación o renovación *')" 
+            type="text" 
+            maxlength="4"  
+            placeholder="Clave" 
+        />
     </div>
 
 
-    <flux:radio.group wire:model="autorizacion" label="¿Autorización para desempeñarse en otra entidad federativa?">
+    <flux:radio.group wire:model="autorizacion" label="¿Autorización para desempeñarse en otra entidad federativa? *">
         <flux:radio value="1" label="Sí" />
         <flux:radio value="2" label="No" />
     </flux:radio.group>
@@ -88,15 +94,19 @@
     <div>
         <div x-show="autorizacion == 1" x-cloak class="mt-4">
             <div class="col-span-1">
-                <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1">Documento que lo
-                    avale</label>
-                <livewire:dropzone wire:model="avale_autorizado" :rules="['mimes:pdf','max:10420']" :multiple="false" />
+                <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1">
+                    Documento que lo avale *
+                </label>
+                <livewire:dropzone 
+                    wire:model="avale_autorizado" 
+                    :rules="['mimes:pdf','max:10420']" 
+                    :multiple="false" 
+                />
             </div>
         </div>
     </div>
 
-    <flux:radio.group wire:model="especializacion"
-        label="¿Cuenta con especialización en justicia restaurativa o terapéutica?">
+    <flux:radio.group wire:model="especializacion" label="¿Cuenta con especialización en justicia restaurativa o terapéutica? *">
         <flux:radio value="1" label="Sí" />
         <flux:radio value="2" label="No" />
     </flux:radio.group>
@@ -104,15 +114,19 @@
     <div>
         <div x-show="especializacion == 1" x-cloak class="mt-4">
             <div class="col-span-1">
-                <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1">Documento que lo
-                    avale</label>
-                <livewire:dropzone wire:model="avale_especializacion" :rules="['mimes:pdf','max:10420']" :multiple="false" />
+                <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1">
+                    Documento que lo avale *
+                </label>
+                <livewire:dropzone 
+                    wire:model="avale_especializacion" 
+                    :rules="['mimes:pdf','max:10420']" 
+                    :multiple="false"
+                />
             </div>
         </div>
     </div>
 
-    <flux:radio.group wire:model="especializacion_arbitra"
-        label="¿Cuenta con especialización para desempeñarse como persona árbitra?">
+    <flux:radio.group wire:model="especializacion_arbitra" label="¿Cuenta con especialización para desempeñarse como persona árbitra? *">
         <flux:radio value="1" label="Sí" />
         <flux:radio value="2" label="No" />
     </flux:radio.group>
@@ -120,10 +134,14 @@
     <div>
         <div x-show="especializacion_arbitra == 1" x-cloak class="mt-4">
             <div class="col-span-1">
-                <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1">Documento que lo
-                    avale</label>
-                <livewire:dropzone wire:model="avale_autorizado_arbitra" :rules="['mimes:pdf','max:10420']"
-                    :multiple="false" />
+                <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1">
+                    Documento que lo avale *
+                </label>
+                <livewire:dropzone 
+                    wire:model="avale_autorizado_arbitra" 
+                    :rules="['mimes:pdf','max:10420']"
+                    :multiple="false" 
+                />
             </div>
         </div>
     </div>
@@ -134,34 +152,50 @@
 <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-6" x-data="{
         tiene_resolucion: @entangle('tiene_resolucion').live,
     }">
-    <flux:input wire:model="convenios_suscritos" :label="__('Número de convenios suscritos')" type="number" required
-        placeholder="Número de convenios suscritos" />
 
-    <flux:input wire:model="convenios_ejecutados" :label="__('Número de convenios ejecutados vía de apremio')"
-        type="number" required placeholder="Número de convenios ejecutados vía de apremio" />
+    <flux:input 
+        wire:model="convenios_suscritos" 
+        :label="__('Número de convenios suscritos *')" 
+        type="number" 
+        placeholder="Número de convenios suscritos" 
+    />
 
-  
+    <flux:input 
+        wire:model="convenios_ejecutados" 
+        :label="__('Número de convenios ejecutados vía de apremio *')"
+        type="number" 
+        placeholder="Número de convenios ejecutados vía de apremio"
+    />
 
-    <flux:radio.group wire:model="tiene_resolucion" label="¿Tiene resolución?">
+    <flux:radio.group wire:model="tiene_resolucion" label="¿Tiene resolución? *">
         <flux:radio value="1" label="Sí" />
         <flux:radio value="2" label="No" />
     </flux:radio.group>
 
     <div>
         <div x-show="tiene_resolucion == 1" x-cloak class="mt-1 col-span-2">
-            <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200">Resolución:</label>
-            <livewire:dropzone wire:model="avale_resolucion" :rules="['mimes:pdf','max:10420']" :multiple="false" />
+            <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200">Resolución: *</label>
+            <livewire:dropzone 
+                wire:model="avale_resolucion" 
+                :rules="['mimes:pdf','max:10420']" 
+                :multiple="false" 
+            />
         </div>
     </div>
         
     <div class="col-span-2">
-        <flux:input wire:model="infracciones" :label="__('Infracciones cometidas')" type="text" required
-            placeholder="Infracciones cometidas" />
+        <flux:input
+            wire:model="infracciones" 
+            :label="__('Infracciones cometidas *')" 
+            type="text" 
+            placeholder="Infracciones cometidas" 
+        />
     </div>
 </div>
 
+<!-- Sección 4: Descripcion y cancelacion -->
 <div class="mt-1 grid grid-cols-2">
-    <flux:radio.group wire:model="descripcion_sancion" label="Descripción de Sanciones impuestas, en su caso">
+    <flux:radio.group wire:model="descripcion_sancion" label="Descripción de Sanciones impuestas, en su caso *">
         <flux:radio value="1" label="Amonestación" />
         <flux:radio value="2" label="Sanción económica" />
         <flux:radio value="3" label="Reparación del Daño" />
@@ -170,7 +204,7 @@
         <flux:radio value="6" label="Inhabilitación" />
     </flux:radio.group>
 
-    <flux:radio.group wire:model="cancelacion" label="Cancelación de Registro">
+    <flux:radio.group wire:model="cancelacion" label="Cancelación de Registro *">
         <flux:radio value="1" label="Suspensión" />
         <flux:radio value="2" label="Revocación" />
         <flux:radio value="3" label="Inhabilitación" />
@@ -181,9 +215,8 @@
     </flux:radio.group>
 </div>
 
-
-<!-- Sección 4: Documentos clave -->
-<div class="flex flex-col mt-6">
+<!-- Sección 5: Documentos clave -->
+<div class="flex flex-col mt-6 text-sm">
     <flux:link href="#">Hipervínculo de certificación inicial</flux:link>
     <flux:link href="#">Hipervínculo a Vigencia</flux:link>
     <flux:link href="#">Hipervínculo a Acuerdo Plenario (Renovación)</flux:link>
@@ -196,63 +229,88 @@
         dictamen_cja: @entangle('dictamen_cja').live,
         juicio_amparo: @entangle('juicio_amparo').live
     }">
-    <flux:radio.group wire:model="elementos_materiales" label="Elementos materiales para el ejercicio de su función">
+
+    <flux:radio.group wire:model="elementos_materiales" label="Elementos materiales para el ejercicio de su función *">
         <flux:radio value="1" label="Registro de sello, rúbrica o media firma y firma ante el R.P.P.C.CDMX" />
         <flux:radio value="2" label="Registro ante SAT" />
     </flux:radio.group>
 
     <div>
-        <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1 mt-1">Documento que lo avale</label>
+        <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1 mt-1">Documento que lo avale *</label>
         <livewire:dropzone wire:model="avale_materiales" :rules="['mimes:mp4,pdf','max:10420']" :multiple="true" />
     </div>
 
-    <flux:radio.group wire:model="visitas_supervision" label="¿Recibió visitas de supervisión?">
+    <flux:radio.group wire:model="visitas_supervision" label="¿Recibió visitas de supervisión? *">
         <flux:radio value="1" label="Sí" />
         <flux:radio value="2" label="No" />
     </flux:radio.group>
 
     <div>
         <div x-show="visitas_supervision == 1" x-cloak class="mt-1 col-span-2">
-            <flux:input wire:model="fecha_supervision" :label="__('Fecha de la supervisión')" type="date" required
-                placeholder="Fecha de la supervisión" />
+            <flux:input 
+                wire:model="fecha_supervision" 
+                :label="__('Fecha de la supervisión *')" 
+                type="date" 
+                placeholder="Fecha de la supervisión" 
+            />
             <div>
-                <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1 mt-1">Añadir video y/o
-                    acta de
-                    visita:</label>
+                <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1 mt-1">
+                    Añadir video y/o acta de visita: *
+                </label>
                 <livewire:dropzone wire:model="video_supervision" :rules="['mimes:mp4,pdf','max:10420']" :multiple="true" />
             </div>
         </div>
     </div>
 
-    <flux:input wire:model="fecha_publicacion"
-        :label="__('Fecha de publicación de la certificación o renovación en Boletín Judicial o Gaceta Oficial de la CDMX')"
-        type="date" required />
+    <flux:input 
+        wire:model="fecha_publicacion"
+        :label="__('Fecha de publicación de la certificación o renovación en Boletín Judicial o Gaceta Oficial de la CDMX *')"
+        type="date" 
+    />
     
     <div>
-        <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1 mt-1">Documento que lo
-            avale</label>
-        <livewire:dropzone wire:model="publicacion_documento" :rules="['mimes:mp4,pdf','max:10420']" :multiple="true" />
+        <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1 mt-1">
+            Documento que lo avale *
+        </label>
+        <livewire:dropzone 
+            wire:model="publicacion_documento" 
+            :rules="['mimes:mp4,pdf','max:10420']" 
+            :multiple="true" 
+        />
     </div>
 
-    <flux:radio.group wire:model="juicio_amparo" label="¿Jucio Amparo?">
+    <flux:radio.group wire:model="juicio_amparo" label="¿Jucio Amparo? *">
        <flux:radio value="1" label="Sí" />
        <flux:radio value="2" label="No" />
     </flux:radio.group>
     <div>
        <div x-show="juicio_amparo == 1" x-cloak class="mt-1 col-span-2">
-           <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1 mt-1">Documento que lo avale</label>
-           <livewire:dropzone wire:model="avale_jucio" :rules="['mimes:mp4,pdf','max:10420']" :multiple="true" />
+            <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1 mt-1">
+                Documento que lo avale *
+            </label>
+            <livewire:dropzone 
+                wire:model="avale_jucio" 
+                :rules="['mimes:mp4,pdf','max:10420']" 
+                :multiple="true"
+            />
        </div>
     </div>
    
-    <flux:radio.group wire:model="dictamen_cja" label="Dictamen CJA">
+    <flux:radio.group wire:model="dictamen_cja" label="Dictamen CJA *">
        <flux:radio value="1" label="Sí" />
        <flux:radio value="2" label="No" />
     </flux:radio.group>
+
     <div>
        <div x-show="dictamen_cja == 1" x-cloak class="mt-1 col-span-2">
-           <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1 mt-1">Añadir Dictamen</label>
-           <livewire:dropzone wire:model="avale_dictamen" :rules="['mimes:mp4,pdf','max:10420']" :multiple="true" />
+            <label class="block text-sm font-medium text-zinc-700 dark:text-zinc-200 mb-1 mt-1">
+            Añadir Dictamen *
+            </label>
+            <livewire:dropzone 
+                wire:model="avale_dictamen" 
+                :rules="['mimes:mp4,pdf','max:10420']" 
+                :multiple="true" 
+            />
        </div>
     </div>
 </div>
--- resources/views/livewire/facilitadores.blade.php
+++ resources/views/livewire/facilitadores.blade.php
@@ -1,13 +1,11 @@
 <div
     class="relative h-full place-items-center px-4 bg-gradient-to-br from-white to-gray-100 dark:from-gray-800 dark:to-gray-900 overflow-hidden">
     <div
-        class="absolute w-[500px] h-[500px] bg-emerald-500/40 blur-[90px] dark:blur-[120px] rounded-full top-1/2 right-1/2 -translate-x-1/2 -translate-y-1/2 z-0 animate__animated animate__bounceIn">
+        class="absolute w-[500px] h-[500px] bg-emerald-500/40 blur-[90px] dark:blur-[120px] rounded-full top-1/2 right-1/2 -translate-x-1/2 -translate-y-1/2 z-0">
     </div>
 
-    <div
-        class="relative mt-2 mb-2 z-10 w-full max-w-4xl rounded-2xl shadow-2xl ring-1 ring-gray-200 dark:ring-gray-700 bg-white dark:bg-gray-900 p-6 sm:p-8 lg:p-10 animate__animated animate__fadeIn">
+    <div class="relative mt-2 mb-2 z-10 w-full max-w-4xl rounded-2xl shadow-2xl ring-1 ring-gray-200 dark:ring-gray-700 bg-white dark:bg-gray-900 p-6 sm:p-8 lg:p-10">
 
-        <!-- Navbar como Tab Bar -->
         <flux:navbar class="mb-6 justify-center">
             <flux:navbar.item wire:click="cambiarTab(1)" icon="document" :current="$tab === 1">
                 Datos generales
@@ -18,52 +16,18 @@
             </flux:navbar.item>
         </flux:navbar>
 
-
         <div wire:loading wire:target='cambiarTab' class="mb-6 w-full mx-auto">
             @include('components.convenio.includes.skeleton-loader')
         </div>
 
-        <!-- Contenido del tab -->
         <div class="p-4 rounded-md  dark:bg-gray-900 " 
             x-data="{tipo: @entangle('tipo').live}"
-            wire:loading.remove wire:target='cambiarTab'    
-        >
+            wire:loading.remove wire:target='cambiarTab'>
             @if ($tab === 1)
-            <div class=" grid grid-cols-4 gap-4 mb-4" >
-                <flux:radio.group wire:model="tipo" label="Persona facilitadora">
-                    <flux:radio value="1" label="Público" />
-                    <flux:radio value="2" label="Privado" />
-                </flux:radio.group>
-
-                <flux:radio.group wire:model="materia" label="Materias Certificado">
-                    <flux:radio value="1" label="Civil-Mercantil " />
-                    <flux:radio value="2" label="Familiar" />
-                    <flux:radio value="3" label="Ambas" />
-                </flux:radio.group>
-
-                <flux:radio.group wire:model="estudios" label="Grado de estudios">
-                    <flux:radio value="1" label="Licenciatura" />
-                    <flux:radio value="2" label="Maestria" />
-                    <flux:radio value="3" label="Doctorado" />
-                </flux:radio.group>
-
-                <flux:input
-                    wire:model="cedula"
-                    :label="__('Número de Cédula Profesional')"
-                    type="text"
-                    required
-                    placeholder="Número de Cédula Profesional, expedida por la Dirección General de Profesiones"
-                />
-            </div>
-
-                <div x-show="tipo !== ''" x-cloak class="mt-4">
-                    @include('facilitadores.formulario.datosGenerales')
-                </div>
+                @include('facilitadores.formulario.datosGenerales')
             @elseif ($tab === 2)
-        
-                @include('facilitadores.formulario.publico')
+                @include('facilitadores.formulario.datosAdicionales')
             @endif
-
         </div>
 
         <div class="mt-10 pt-6 border-t border-neutral-200 dark:border-neutral-700 flex justify-between items-center">
@@ -80,7 +44,6 @@
                     Siguiente
                 </flux:button>
             @endif
-            
    
             @if ($tab === 2)
             <flux:button wire:click="save"  variant='primary'>
